c40cfda4269b5f93e971cb2745e017428654fd7e
JoaoLendengues <joaotiprojetos@gmail.com>
2025-03-26 12:03:11 -0300

    Update conexao.py

--- db/conexao.py
+++ db/conexao.py
@@ -4,10 +4,10 @@ from app_web.config import MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB
 def conectar():
     try:
         conn = mysql.connector.connect(
-            host="10.1.1.9",
-            user="Joao",
-            password="@Jvpdata03",
-            database="sistema_candidaturas"
+            host="",
+            user="",
+            password="",
+            database=""
         )
         return conn
     except mysql.connector.Error as err:
